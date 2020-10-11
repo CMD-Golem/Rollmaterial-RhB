f50a31f1b9d2bc9bd4279a58010fde2fd7979744
CMD-Golem <64982383+CMD-Golem@users.noreply.github.com>
2020-10-11 17:21:46 +0200

    Updated Links
    Navbar, Footer and Missing Image

--- elements/nav/footer.js
+++ elements/nav/footer.js
@@ -1,4 +1,4 @@
-document.write('<footer><div class="footer_content"><div class="footer_flex"><div style="flex: 40%"><a style="margin-right: 50px;" href="info/impressum.html">Impressum</a> <a style="margin-right: 50px;" href="info/datenschutz.html">Datenschutz</a> <a href="info/anfragen.html">Anfragen</a></div><div style="flex: 40%"><p style="font-size: 12px; text-align: right;">©<script>document.write(new Date().getFullYear());</script> by Tobias und Fabian Kaufmann</p></div></div><div style="height:20px;"></div><p style="font-size: 12px; text-align: center;">Alle Bilder auf dieser Webseite sind Urheberrechtlich gesch&uuml;tzt und d&uuml;rfen ohne ein ausdr&uuml;ckliches Erlaubnis des Betreibers nicht weiterverwendet werden.</p><p style="font-size: 12px; text-align: center;">Diese Webseite ist rein Privat und nicht mit der Rh&auml;tische Bahn verbunden.</p></div></footer>');
+document.write('<footer><div class="footer_content"><div class="footer_flex"><div style="flex: 40%"><a style="margin-right: 50px;" href="https://rollmaterial-rhb.netlify.app/info/impressum.html">Impressum</a> <a style="margin-right: 50px;" href="https://rollmaterial-rhb.netlify.app/info/datenschutz.html">Datenschutz</a> <a href="info/anfragen.html">Anfragen</a></div><div style="flex: 40%"><p style="font-size: 12px; text-align: right;">©<script>document.write(new Date().getFullYear());</script> by Tobias und Fabian Kaufmann</p></div></div><div style="height:20px;"></div><p style="font-size: 12px; text-align: center;">Alle Bilder auf dieser Webseite sind Urheberrechtlich gesch&uuml;tzt und d&uuml;rfen ohne ein ausdr&uuml;ckliches Erlaubnis des Betreibers nicht weiterverwendet werden.</p><p style="font-size: 12px; text-align: center;">Diese Webseite ist rein Privat und nicht mit der Rh&auml;tische Bahn verbunden.</p></div></footer>');
 
 // Add title to not definitive Data
 var notdef = document.getElementsByTagName("u");
@@ -20,7 +20,7 @@ function fixBrokenImages(url) {
         var t = img[i];
         if(t.naturalWidth === 0){
             //this image is broken
-            t.src = "https://raw.githubusercontent.com/CMD-Golem/cmd-golem.github.io/master/elements/noimage.png";
+            t.src = "https://rollmaterial-rhb.netlify.app/elements/noimage.png";
         }
     }
 }
