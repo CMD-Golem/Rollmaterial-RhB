5ce3bd31b514086f5cb1acc71fc56122b09faf3a
CMD-Golem <64982383+CMD-Golem@users.noreply.github.com>
2020-10-15 22:31:32 +0200

    Cleande up style files

--- elements/javascript/background.js
+++ elements/javascript/background.js
@@ -1,6 +1,6 @@
-var numimages=3;
+var numimages=4;
 
-rndimg = new Array("elements/background/1.jpg", "elements/background/2.jpg", "elements/background/3.jpg"); 
+rndimg = new Array("elements/background/1.jpg", "elements/background/2.jpg", "elements/background/3.jpg", "elements/background/4.jpg"); 
 x = (Math.floor(Math.random()*numimages));
 randomimage=(rndimg[x]);
 document.getElementById("background").style.backgroundImage = "url("+ randomimage +")"; 
