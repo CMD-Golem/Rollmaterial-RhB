e10d584685373533596725a89a91bf38ab820dfb
CMD-Golem <64982383+CMD-Golem@users.noreply.github.com>
2020-10-17 12:16:46 +0200

    Updated Style

--- elements/javascript/site_search.js
+++ elements/javascript/site_search.js
@@ -27,34 +27,3 @@ function siteSearch() {
     }
   }
 }
-
-
-function siteSearchMobile() {
-  var input, filter, table, elements, keyword, i, group;
-  input = document.getElementById("Site_Search_Mobile");
-  filter = input.value.toUpperCase();
-  table = document.getElementById("data_table");
-  elements = table.getElementsByTagName("tr");
-  group = table.getElementsByClassName("group");
-
-
-  for (i = 0; i < elements.length; i++) {
-    keyword = elements[i].getElementsByClassName("table_6")[0];
-    if (keyword.innerHTML.toUpperCase().indexOf(filter) > -1) {
-      elements[i].style.display = "";
-    }
-    else {
-      elements[i].style.display = "none";
-    }
-  }
-  
-  for (i = 0; i < group.length; i++) {
-    keyword2 = group[i].getElementsByClassName("list")[0];
-    if (keyword2.innerHTML.toUpperCase().indexOf(filter) > -1) {
-      group[i].style.display = "";
-    }
-    else {
-      group[i].style.display = "none";
-    }
-  }
-}
