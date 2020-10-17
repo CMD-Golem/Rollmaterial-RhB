9171191f0fc2a2ed32c21c1a4783067d9fce5a22
CMD-Golem <64982383+CMD-Golem@users.noreply.github.com>
2020-10-17 14:16:34 +0200

    fix navbar on small screens

--- elements/nav/footer.js
+++ elements/nav/footer.js
@@ -1,5 +1,16 @@
 document.write('<footer><div class="footer_text"><div class="footer_links"><a href="https://rollmaterial-rhb.netlify.app/info/impressum.html">Impressum</a> <a href="https://rollmaterial-rhb.netlify.app/info/datenschutz.html">Datenschutz</a> <a href="">Anfragen</a> <a href="">Fehler melden</a></div><div class="footer_copyright">©<script>document.write(new Date().getFullYear());</script> by Tobias und Fabian Kaufmann</div></div><div style="height:20px;"></div><p>Alle Bilder auf dieser Webseite sind Urheberrechtlich gesch&uuml;tzt und d&uuml;rfen ohne schriftliche Erlaubnis des Betreibers nicht weiterverwendet werden.</p><p>Diese Webseite ist rein Privat und nicht mit der Rh&auml;tische Bahn verbunden.</p></footer>');
 
+//navbar
+function MenuFunction() {
+  var x = document.getElementById("Menu");
+  if (x.className === "nav") {
+    x.className += " nav_resp";
+  } else {
+    x.className = "nav";
+  }
+}
+
+
 // Add title to not definitive Data
 var notdef = document.getElementsByTagName("u");
 for (i = 0; i < notdef.length; i++) {
@@ -16,11 +27,18 @@ window.onload = fixBrokenImages;
 function fixBrokenImages(url) {
     var img = document.getElementsByTagName('img');
     var i=0, l=img.length;
-    for(;i<l;i++){
+    for (;i<l;i++) {
         var t = img[i];
         if(t.naturalWidth === 0){
             //this image is broken
             t.src = "https://rollmaterial-rhb.netlify.app/elements/noimage.png";
         }
     }
+}
+
+//set width
+
+var mainwidth = document.getElementsByTagName("main")[0].offsetWidth;
+if (mainwidth <= 400) {
+    document.getElementsByTagName("nav")[0].setAttribute("style", "width:" + mainwidth + "px");
 }
