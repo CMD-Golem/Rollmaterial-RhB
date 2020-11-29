8a6b5a3c66d4ff93256c01f3c7b09b32d55c53f4
CMD-Golem <64982383+CMD-Golem@users.noreply.github.com>
2020-11-29 13:32:30 +0100

    bug fix

--- elements/nav/navbar.js
+++ elements/nav/navbar.js
@@ -3,7 +3,7 @@ const navbar = `
 	<input type="checkbox" id="menu">
 	<label for="menu" class="nav_menu nav_hover"></label>
 	<a class="nav_home disable_link nav_hover" href="https://rollmaterial-rhb.netlify.app/"><img src="../../elements/nav/home.svg"><p>Home</p></a>
-	<a class="nav_search disable_link nav_hover" href="https://rollmaterial-rhb.netlify.app/data.html/"><img src="../../elements/nav/search.svg"></a>
+	<a class="nav_search disable_link nav_hover" href="https://rollmaterial-rhb.netlify.app/data.html"><img src="../../elements/nav/search.svg"></a>
 	<ul class="nav_list">
 		<li><a class="nav_link disable_link nav_hover" href="https://rollmaterial-rhb.netlify.app/data/lokomotiven.html" id="lokomotiven">Lokomotiven</a></li>
 		<li><a class="nav_link disable_link nav_hover" href="https://rollmaterial-rhb.netlify.app/data/personenwagen.html" id="personenwagen">Personenwagen</a></li>
