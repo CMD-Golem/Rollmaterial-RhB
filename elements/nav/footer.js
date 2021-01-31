49b5b4f4bed8b10c2bdc845911dac7dc7171261a
CMD-Golem <64982383+CMD-Golem@users.noreply.github.com>
2021-01-31 19:11:22 +0100

    Added Barnina Fahrzeuge, New Impressum

--- elements/nav/footer.js
+++ elements/nav/footer.js
@@ -3,12 +3,11 @@ const footer = `
 	<div class="footer_text">
 		<div class="footer_links">
 			<a href="https://rollmaterial-rhb.netlify.app/info/impressum.html">Impressum</a>
-			<a href="https://rollmaterial-rhb.netlify.app/info/datenschutz.html">Datenschutz</a>
 			<a href="https://rollmaterial-rhb.netlify.app/info/aenderungen.html">Änderungen</a>
 			<a href="https://rollmaterial-rhb.netlify.app/info/anfragen.html">Anfragen</a>
 			<a href="https://rollmaterial-rhb.netlify.app/info/melden.html">Fehler melden</a>
 		</div>
-		<div class="footer_copyright">©<script>document.write(new Date().getFullYear());</script> by Tobias und Fabian Kaufmann</div>
+		<div class="footer_copyright">©2020 - <script>document.write(new Date().getFullYear());</script> by Tobias und Fabian Kaufmann</div>
 	</div>
 	<div style="height:20px;"></div>
 	<p>Alle Bilder auf dieser Webseite sind Urheberrechtlich gesch&uuml;tzt und d&uuml;rfen ohne schriftliche Erlaubnis des Betreibers nicht weiterverwendet werden.</p>
