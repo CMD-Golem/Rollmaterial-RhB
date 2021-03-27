6d9dd88e7eeb988749fc7f82b984c10d17a64ae1
CMD-Golem <64982383+CMD-Golem@users.noreply.github.com>
2021-03-27 19:39:20 +0100

    Edit Mode Update

--- elements/javascript/spoiler.js
+++ elements/javascript/spoiler.js
@@ -2,13 +2,13 @@ var acc = document.getElementsByClassName("spoiler");
 var i;
 
 for (i = 0; i < acc.length; i++) {
-  acc[i].onclick = function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight){
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    } 
-  }
+	acc[i].onclick = function() {
+		this.classList.toggle("active");
+		var panel = this.nextElementSibling;
+		if (panel.style.maxHeight){
+			panel.style.maxHeight = null;
+		} else {
+			panel.style.maxHeight = panel.scrollHeight + "px";
+		} 
+	}
 }
--- elements/nav/navbar.js
+++ elements/nav/navbar.js
@@ -2,8 +2,8 @@ const navbar = `
 <nav>
 	<input type="checkbox" id="menu">
 	<label for="menu" class="nav_menu nav_hover"></label>
-	<a class="nav_home disable_link nav_hover" href="https://rollmaterial-rhb.netlify.app/"><img src="https://rollmaterial-rhb.netlify.app/elements/nav/home.svg"><p>Home</p></a>
-	<a class="nav_search disable_link nav_hover" href="https://rollmaterial-rhb.netlify.app/data.html"><img src="https://rollmaterial-rhb.netlify.app/elements/nav/search.svg"></a>
+	<a class="nav_home disable_link nav_hover" href="https://rollmaterial-rhb.netlify.app/" title="Home"><img src="https://rollmaterial-rhb.netlify.app/elements/favicon.png"><p>Rollmaterial-RhB</p></a>
+	<a class="nav_search disable_link nav_hover" href="https://rollmaterial-rhb.netlify.app/data.html" title="Suchen"><img src="https://rollmaterial-rhb.netlify.app/elements/nav/search.svg"></a>
 	<ul class="nav_list">
 		<li><a class="nav_link disable_link nav_hover" href="https://rollmaterial-rhb.netlify.app/data/lokomotiven.html" id="lokomotiven">Lokomotiven</a></li>
 		<li><a class="nav_link disable_link nav_hover" href="https://rollmaterial-rhb.netlify.app/data/personenwagen.html" id="personenwagen">Personenwagen</a></li>
