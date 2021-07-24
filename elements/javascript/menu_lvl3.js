647b5cc3cfeab84e788c74b953a2118f6fa5ef0f
CMD-Golem <64982383+CMD-Golem@users.noreply.github.com>
2021-07-24 17:55:31 +0200

    Updated Spoiler, Print view, bug fixes

--- elements/javascript/menu_lvl3.js
+++ elements/javascript/menu_lvl3.js
@@ -1,27 +1,32 @@
 // Spoiler
-var acc = document.getElementsByClassName("spoiler");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-	acc[i].onclick = function() {
-		this.classList.toggle("active");
-		var panel = this.nextElementSibling;
-		if (panel.style.maxHeight){
-			panel.style.maxHeight = null;
-		} else {
-			panel.style.maxHeight = panel.scrollHeight + "px";
-		} 
-	}
+var el_spoiler = document.getElementsByClassName("spoiler");
+for (var i = 0; i < el_spoiler.length; i++) {
+	el_spoiler[i].addEventListener("click", spoiler);
 }
+
+function spoiler(selected_spoiler) {
+	selected_spoiler.target.classList.toggle("active");
+	var spoiler_content = selected_spoiler.target.nextElementSibling;
+
+	if (spoiler_content.style.maxHeight) {
+		spoiler_content.lastElementChild.style.marginBottom = null;
+		spoiler_content.style.maxHeight = null;
+	}
+	else {
+		spoiler_content.lastElementChild.style.marginBottom = "50px"
+		spoiler_content.style.maxHeight = spoiler_content.scrollHeight + "px";
+	} 
+}
+
 
 // Header img
 var header_img = document.getElementsByClassName("left_space")[0].getElementsByTagName("img")[0].src
 
 function headerImg() {
-	if (window.innerWidth < 1200) {
+	if (window.innerWidth <= 1200) {
 		document.getElementById("header").style.backgroundImage = "url('" + header_img + "')";
 	}
-	if (window.innerWidth >= 1200) {
+	if (window.innerWidth > 1200) {
 		document.getElementById("header").style.backgroundImage = "";
 	}
 }
