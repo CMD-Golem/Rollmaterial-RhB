593d9dce11e4916445b55eabe759408f5dfffa13
CMD-Golem <64982383+CMD-Golem@users.noreply.github.com>
2021-08-28 16:05:04 +0200

    faunadb test

--- netlify/functions/update.js
+++ netlify/functions/update.js
@@ -9,32 +9,21 @@ exports.handler = (event, context) => {
   });
 
   // get document id and collection name from url
+  var data = JSON.parse(event.body);
   var id = event.path.match(/([^\/]*)\/*$/)[0];
   var collection = event.path.replace("/" + id, "").match(/([^\/]*)\/*$/)[0];
 
-  // get data from db
-  return client.query(q.Get(q.Ref(`classes/${collection}/${id}`))).then((current) => {
-
-    // update counter
-    var data = {
-      count: JSON.parse(JSON.stringify(current)).data.count + 1,
-      date: new Date()
-    }
-    
-    data = JSON.parse(JSON.stringify(data));
-
-    // update data in db
-    return client.query(q.Update(q.Ref(`classes/${collection}/${id}`), {data}))
-      .then((response) => {
-        return {
-          statusCode: 200,
-          body: JSON.stringify(response)
-        }
-      }).catch((error) => {
-        return {
-          statusCode: 400,
-          body: JSON.stringify(error)
-        }
-      })
-    });
+  // update data from db
+  return client.query(q.Update(q.Ref(`classes/${collection}/${id}`), {data}))
+    .then((response) => {
+      return {
+        statusCode: 200,
+        body: JSON.stringify(response)
+      }
+    }).catch((error) => {
+      return {
+        statusCode: 400,
+        body: JSON.stringify(error)
+      }
+    })
 }
