fd7e0ee0ebbb65679470bd4b1def2d98b89c5cbb
CMD-Golem <64982383+CMD-Golem@users.noreply.github.com>
2021-08-28 15:50:57 +0200

    faunadb test

---
+++ elements/javascript/test.js
@@ -0,0 +1,48 @@
+// read db
+var read = (id, collection) => {
+  return fetch(`/.netlify/functions/read/${collection}/${id}`, {
+    method: 'POST',
+  }).then(response => {
+    return response.json();
+  })
+}
+
+// update db
+var update = (id, collection) => {
+  return fetch(`/.netlify/functions/update/${collection}/${id}`, {
+    method: 'POST',
+  }).then(response => {
+    return response.json();
+  })
+}
+
+
+// ###########################################################
+// set id and collection name of counter
+var id = "308175711959515721";
+var collection = "counter";
+
+
+// show counter in html
+Promise.resolve( read(id, collection) ).then( function(value) {
+  try {
+    document.getElementById("show_counter").innerHTML = value.data.count;
+  }
+  catch (e) {
+    console.log("Counter isn't shown on page");
+  }
+});
+
+
+// update counter
+var already_download = false;
+
+function updateCounter() {
+  if (already_download != true) {
+    already_download = true;
+    update(id, collection)//.then((value) => { console.log(value); });
+  }
+  else {
+    console.log("Already downloaded");
+  }
+}
